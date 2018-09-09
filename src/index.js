d53013bfbee578bf35e6a17d0a247c068c602de3
Diego Marcos <diego.marcos@gmail.com>
2018-09-09 16:45:45 -0700

    bump dist

--- src/index.js
+++ src/index.js
@@ -80,7 +80,7 @@ require('./core/a-mixin');
 require('./extras/components/');
 require('./extras/primitives/');
 
-console.log('A-Frame Version: 0.8.2 (Date 2018-07-26, Commit #b6425da)');
+console.log('A-Frame Version: 0.8.2 (Date 2018-09-09, Commit #12ed6df25)');
 console.log('three Version:', pkg.dependencies['three']);
 console.log('WebVR Polyfill Version:', pkg.dependencies['webvr-polyfill']);
 

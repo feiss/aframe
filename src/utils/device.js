6a1691c40dc152b4ebf9620227d1237e5a777893
Diego Marcos <diego.marcos@gmail.com>
2018-11-12 17:36:47 -0800

    Add WebXR support

--- src/utils/device.js
+++ src/utils/device.js
@@ -1,10 +1,23 @@
 var vrDisplay;
+var webXRAvailable = false;
 
-if (navigator.getVRDisplays) {
-  navigator.getVRDisplays().then(function (displays) {
-    vrDisplay = displays.length && displays[0];
+// Support both WebVR and WebXR APIs.
+if (navigator.xr) {
+  navigator.xr.requestDevice().then(function (device) {
+    device.supportsSession({immersive: true, exclusive: true}).then(function () {
+      vrDisplay = device;
+      webXRAvailable = true;
+    });
   });
+} else {
+  if (navigator.getVRDisplays) {
+    navigator.getVRDisplays().then(function (displays) {
+      vrDisplay = displays.length && displays[0];
+    });
+  }
 }
+
+module.exports.isWebXR = function isWebXR () { return webXRAvailable; };
 
 function getVRDisplay () { return vrDisplay; }
 module.exports.getVRDisplay = getVRDisplay;
